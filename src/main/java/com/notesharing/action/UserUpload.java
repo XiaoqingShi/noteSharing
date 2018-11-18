179e3fde9479cc72a573e4b86c9e6d4168f88b57
XiaoqingShi <994338972@qq.com>
2018-11-18 21:52:47 +0800

    上传用户头像并修改头像地址

--- src/main/java/com/notesharing/action/UserUpload.java
+++ src/main/java/com/notesharing/action/UserUpload.java
@@ -1,87 +1,103 @@
 package com.notesharing.action;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.notesharing.service.UserServiceI;
 import com.notesharing.superAction.SuperAction;
+import com.opensymphony.xwork2.ActionContext;
 
+@ParentPackage("jsonPackage")
+@Action(value = "userUpload")
+@Namespace("/") // 使用convention-plugin插件提供的@Namespace注解为这个Action指定一个命名空间
+@Results({ @Result(name = "success", type = "json", params = { "root", "dataMap" }),
+		@Result(name = "error", location = "/user/login_failure.jsp") })
 public class UserUpload extends SuperAction {
+	private File image; // 上传的文件
+	private String imageFileName; // 文件名称
+	private String[] imageContentType; // 文件类型
+
 	@Autowired
 	private UserServiceI userService;
-	private File myFile;
+	private Map<String, Object> dataMap;
+	private String userName;
 
-	private String myFileContentType;
-
-	private String myFileFileName;
-
-	private String destPath;
-	List<String> userlist;
-
-	public List<String> getUserlist() {
-		return userlist;
-	}
-
-	public String execute() throws UnknownHostException {
-		userlist = new ArrayList<String>();
-
-		destPath = ServletActionContext.getServletContext().getRealPath("/images");
-		InetAddress address = InetAddress.getLocalHost();// 获取的是本地的IP地址 //PC-20140317PXKX/192.168.0.121
-		String hostAddress = address.getHostAddress();// 192.168.0.121
-		System.out.println(hostAddress);
-		System.out.println(request.getScheme() + "://" + hostAddress + ":" + request.getServerPort()
-				+ request.getContextPath() + "/images/" + myFileFileName);
-		try {
-			System.out.println("Src File name: " + myFile);
-			System.out.println("Dst File name: " + myFileFileName);
-			if (myFileFileName == null)
-				myFileFileName = "1.jpg";
-			File destFile = new File(destPath, myFileFileName);
-			if (myFile == null)
-				return ERROR;
-			FileUtils.copyFile(myFile, destFile);
-			userlist.add("上传成功");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return ERROR;
+	public String execute() throws Exception {
+		dataMap = new HashMap<String, Object>();
+		dataMap.clear();
+		ServletActionContext.getRequest().setCharacterEncoding("UTF-8");
+		String realpath = ServletActionContext.getServletContext().getRealPath("/images");
+		System.out.println(realpath);
+		if (image != null) {
+			File savedir = new File(realpath);
+			if (!savedir.getParentFile().exists())
+				savedir.getParentFile().mkdirs();
+			Date date = new Date();
+			String time = String.valueOf(date.getTime());// 按时间存图片
+			int a = 0;
+			String[] aStrings = imageFileName.split("\\.");
+			// System.out.println(new Date().toString());
+			File savefile = new File(savedir, time + "_" + aStrings[aStrings.length - 1]);
+			String saveName = time + "_" + aStrings[aStrings.length - 1];
+			FileUtils.copyFile(image, savefile);
+			String path = "/user/images/" + saveName;
+			System.out.println(path);
+			userService.updateHeadportrait(userName, path);
 		}
-		String uname = (String) session.getAttribute("loginUserName");
-		String path = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
-				+ request.getContextPath() + "/images/" + myFileFileName;
-		userService.updateHeadportrait(uname, path);
-		userlist.add(path);
+		ActionContext.getContext().put("message", "文件上传成功");
+		dataMap.put("login_info", "上传成功");
 		return "success";
 	}
 
-	public File getMyFile() {
-		return myFile;
+	public File getImage() {
+		return image;
 	}
 
-	public void setMyFile(File myFile) {
-		this.myFile = myFile;
+	public void setImage(File image) {
+		this.image = image;
 	}
 
-	public String getMyFileContentType() {
-		return myFileContentType;
+	public String getImageFileName() {
+		return imageFileName;
 	}
 
-	public void setMyFileContentType(String myFileContentType) {
-		this.myFileContentType = myFileContentType;
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
 	}
 
-	public String getMyFileFileName() {
-		return myFileFileName;
+	public String[] getImageContentType() {
+		return imageContentType;
 	}
 
-	public void setMyFileFileName(String myFileFileName) {
-		this.myFileFileName = myFileFileName;
+	public void setImageContentType(String[] imageContentType) {
+		this.imageContentType = imageContentType;
 	}
+
+	public Map<String, Object> getDataMap() {
+		return dataMap;
+	}
+
+	public void setDataMap(Map<String, Object> dataMap) {
+		this.dataMap = dataMap;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 }
--- src/main/webapp/upload/upload_picture.jsp
+++ src/main/webapp/upload/upload_picture.jsp
@@ -1,14 +1,21 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<base href="<%=basePath%>">
 </head>
 <body>
-	<form action="uploadPhoto" method="post" enctype="multipart/form-data">
+	<form action="userUpload.action" method="post" enctype="multipart/form-data">
 		请选择图片:
-		<input id="myfile" name="myfile" type="file" onchange="showPreview()" />
+		标签1<input type="text" name="userName"><br/>
+		<input id="image" name="image" type="file"  />
 		<input type="submit" value="提交" />
 	</form>
 </body>
